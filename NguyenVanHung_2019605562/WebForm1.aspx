9656b0ad87bc80a4158b7b21e48902abe468e2d0
thai11lab <anhthai2000atm@gmail.com>
2022-07-16 15:04:49 +0700

    first commit

---
+++ NguyenVanHung_2019605562/WebForm1.aspx
@@ -0,0 +1,75 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="NguyenVanHung_2019605562.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 584px;
+        }
+
+        .auto-style2 {
+            width: 342px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table style="width: 100%;">
+                <tr>
+                    <td class="auto-style2">Mã Sản phẩm</td>
+                    <td class="auto-style1">
+                        <asp:TextBox ID="txtMaSP" runat="server" Width="237px"></asp:TextBox></td>
+                    <td>
+                        <asp:Button ID="btnAdd" runat="server" Text="Thêm" OnClick="btnAdd_Click" /></td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">Tên sản phẩm</td>
+                    <td class="auto-style1">
+                        <asp:TextBox ID="txtTenSP" runat="server"></asp:TextBox></td>
+                    <td>
+                        <asp:Button ID="btnSua" runat="server" Text="Sửa" OnClick="btnSua_Click" /></td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">Loại sản phẩm</td>
+                    <td class="auto-style1">
+                        <asp:DropDownList ID="comboLoai" runat="server"></asp:DropDownList></td>
+                    <td>
+                        <asp:Button ID="btnXoa" runat="server" Text="Xóa" OnClick="btnXoa_Click" /></td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">Đơn giá</td>
+                    <td class="auto-style1">
+                        <asp:TextBox ID="txtDonGia" runat="server"></asp:TextBox>   
+                    </td>
+                    <td>
+                        <asp:Button ID="btnTim" runat="server" Text="Tìm" OnClick="btnTim_Click" /></td>
+                </tr>
+            </table>
+        </div>
+        
+        <asp:GridView ID="dgvSanPham" runat="server" AutoGenerateColumns="False" BackColor="White" BorderColor="#CC9966" BorderStyle="None" BorderWidth="1px" CellPadding="4" Width="1307px" OnSelectedIndexChanged="dgvSanPham_SelectedIndexChanged">
+            <Columns>
+                <asp:BoundField DataField="MaSP" HeaderText="Mã Sản Phẩm" />
+                <asp:BoundField DataField="TenSP" HeaderText="Tên Sản Phẩm" />
+                <asp:BoundField DataField="MaLoai" HeaderText="Mã Loại" />
+                <asp:BoundField DataField="Dongia" HeaderText="Đơn Giá" />
+                <asp:CommandField SelectText="Chọn dòng" ShowSelectButton="True" />
+            </Columns>
+            <FooterStyle BackColor="#FFFFCC" ForeColor="#330099" />
+            <HeaderStyle BackColor="#990000" Font-Bold="True" ForeColor="#FFFFCC" />
+            <PagerStyle BackColor="#FFFFCC" ForeColor="#330099" HorizontalAlign="Center" />
+            <RowStyle BackColor="White" ForeColor="#330099" />
+            <SelectedRowStyle BackColor="#FFCC66" Font-Bold="True" ForeColor="#663399" />
+            <SortedAscendingCellStyle BackColor="#FEFCEB" />
+            <SortedAscendingHeaderStyle BackColor="#AF0101" />
+            <SortedDescendingCellStyle BackColor="#F6F0C0" />
+            <SortedDescendingHeaderStyle BackColor="#7E0000" />
+        </asp:GridView>
+        
+    </form>
+</body>
+</html>
---
+++ NguyenVanHung_2019605562/WebForm1.aspx.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace NguyenVanHung_2019605562
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        String strConnectingString = @"Data Source=MAY332\SQLEXPRESS;Initial Catalog=QLBanHang;Integrated Security=True";
+        SqlConnection conn = null;
+        SqlDataAdapter da = null;
+        DataTable dt = null;
+        SqlCommand cmd = null;
+
+        void loadData()
+        {
+            da = new SqlDataAdapter("SELECT * FROM SanPham", conn);
+            dt = new DataTable();
+            da.Fill(dt);
+            dgvSanPham.DataSource = dt;
+            dgvSanPham.DataBind();
+        }
+
+        void loadCombo()
+        {
+            da = new SqlDataAdapter("SELECT * FROM LoaiSP", conn);
+            dt = new DataTable();
+            da.Fill(dt);
+            comboLoai.DataSource = dt;
+            comboLoai.DataTextField = "TenLoai";
+            comboLoai.DataValueField = "MaLoai";
+            comboLoai.DataBind();
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            conn = new SqlConnection(strConnectingString);
+            loadData();
+            if (!IsPostBack) loadCombo();
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            conn = new SqlConnection(strConnectingString);
+            conn.Open();
+
+            string sql = "Insert into SanPham Values (" + txtMaSP.Text + "," + txtTenSP.Text.ToString() + "," + comboLoai.SelectedValue + "," + txtDonGia.Text + ")";
+            cmd = new SqlCommand(sql, conn);
+            cmd.ExecuteNonQuery();
+            loadData();
+        }
+
+        protected void btnSua_Click(object sender, EventArgs e)
+        {
+            conn = new SqlConnection(strConnectingString);
+            conn.Open();
+            string sql = "Update SanPham Set TenSP= '" + txtTenSP.Text.ToString() + "', MaLoai = '" + comboLoai.SelectedValue + "', Dongia ='" + txtDonGia.Text + "' where MaSP ='" + txtMaSP.Text.ToString()+"'";
+            cmd = new SqlCommand(sql, conn);
+            cmd.ExecuteNonQuery();
+            loadData();
+        }
+
+        protected void dgvSanPham_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int dong = dgvSanPham.SelectedIndex;
+            txtMaSP.Text = dgvSanPham.Rows[dong].Cells[0].Text;
+            txtTenSP.Text = dgvSanPham.Rows[dong].Cells[1].Text;
+            comboLoai.SelectedValue = dgvSanPham.Rows[dong].Cells[2].Text;
+            txtDonGia.Text = dgvSanPham.Rows[dong].Cells[3].Text;
+        }
+
+        protected void btnXoa_Click(object sender, EventArgs e)
+        {
+            conn = new SqlConnection(strConnectingString);
+            conn.Open();
+            string sql = "Delete From SanPham where MaSP ='" + txtMaSP.Text.ToString() + "'";
+            cmd = new SqlCommand(sql, conn);
+            cmd.ExecuteNonQuery();
+            loadData();
+        }
+
+        protected void btnTim_Click(object sender, EventArgs e)
+        {
+            conn = new SqlConnection(strConnectingString);
+            conn.Open();
+            string sql = "Select * From SanPham where MaLoai ='" + comboLoai.SelectedValue + "'";
+            
+            da = new SqlDataAdapter(sql, conn);
+            dt = new DataTable();
+            da.Fill(dt);
+            dgvSanPham.DataSource = dt;
+            dgvSanPham.DataBind();
+        }
+    }
+}
